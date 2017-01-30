eb5f392baaf36c99fb1370a857a35ae737d12672
Steven Parker <StevieRay@stevens-mbp.usla.ibm.com>
2017-01-30 10:47:35 -0600

    First commit

---
+++ Touchy/AuthenticationVC.swift
@@ -0,0 +1,98 @@
+//
+//  AuthenticationVC.swift
+//  Touchy
+//
+//  Created by Steven Parker on 10/28/16.
+//  Copyright © 2016 Steven Parker. All rights reserved.
+//
+
+import UIKit
+import LocalAuthentication
+
+class AuthenticationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    @IBAction func useTouchIDButtonWasPressed(_ sender: AnyObject) {
+        let authenticationContext = LAContext()
+        var error: NSError?
+        
+        if authenticationContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            authenticationContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Only humans allowed! Sorry no dogs! 🐶", reply: { (success, error) in
+                
+                if success {
+                    self.navigateToAuthenticatedVC()
+                } else {
+                    if let error = error as? NSError {
+                        let message = self.errorMessageForLAErrorCode(errorCode: error.code)
+                        self.showAlertViewAfterEvaluatingPolicyWithMessage(message: message)
+                    }
+                }
+            })
+        } else {
+            showAlertViewForNoBiometrics()
+            return
+        }
+        
+    }
+    
+    func showAlertViewAfterEvaluatingPolicyWithMessage(message: String) {
+        showAlertWithTitle(title: "Error", message: message)
+    }
+    
+    func errorMessageForLAErrorCode(errorCode: Int) -> String {
+        var message = ""
+        
+        switch errorCode {
+        case LAError.appCancel.rawValue:
+            message = "Authentication was cancelled by application"
+        case LAError.authenticationFailed.rawValue:
+            message = "The user failed tp provide valid credentials"
+        case LAError.invalidContext.rawValue:
+            message = "The context is invalid"
+        case LAError.passcodeNotSet.rawValue:
+            message = "Passcode is not set on the device"
+        case LAError.systemCancel.rawValue:
+            message = "Authenication was cancelled by the system"
+        case LAError.touchIDLockout.rawValue:
+            message = "Too many failed attempts"
+        case LAError.touchIDNotAvailable.rawValue:
+            message = "TouchID is not avaiable on the device"
+        case LAError.userCancel.rawValue:
+            message = "The user did cancel"
+        case LAError.userFallback.rawValue:
+            message = "The user chose to use the fallback"
+        default:
+            message = "Did not find error code on LAError object"
+        }
+        return message
+    }
+    
+    func navigateToAuthenticatedVC() {
+        if let loggedInVC = storyboard?.instantiateViewController(withIdentifier: "LoggedInVC") {
+            DispatchQueue.main.async {
+                self.navigationController?.pushViewController(loggedInVC, animated: true)
+            }
+        }
+    }
+    
+    func showAlertViewForNoBiometrics() {
+        showAlertWithTitle(title: "Error", message: "This device does not have a Touch ID sensor")
+    }
+    
+    func showAlertWithTitle(title: String, message: String) {
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alertVC.addAction(okAction)
+        
+        DispatchQueue.main.async {
+            self.present(alertVC, animated: true, completion: nil)
+        }
+    }
+}
+
---
+++ Touchy/RoundedOutlineView.swift
@@ -0,0 +1,42 @@
+//
+//  RoundedOutlineButton.swift
+//  Touchy
+//
+//  Created by Steven Parker on 10/28/16.
+//  Copyright © 2016 Steven Parker. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedOutlineButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 0.0 {
+        didSet {
+            setupView()
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0.0 {
+        didSet {
+            setupView()
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            setupView()
+        }
+    }
+    
+    func setupView() {
+        layer.cornerRadius = cornerRadius
+        layer.borderWidth = borderWidth
+        layer.borderColor = borderColor?.cgColor
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        setupView()
+    }
+
+}
